cc03d48d39b73f4a15fe3cdb83a3f8015d54343d
MrMrThing <rasmus031299@gmail.com>
2021-11-19 18:53:49 +0100

    Last updates and comments

--- functions.py
+++ functions.py
@@ -2,39 +2,45 @@ import pygame
 import random
 from pygame.display import update
 
-platform_height = [10 for x in range(10)]
-playform_width = [random.randint(50,100) for x in range(10)]
+# Here we get all the variables and draw the platforms and player
+def draw(screen, x, y, player_width, player_height, platform_height, platform_width, platform_X, platform_y, platform_speed):
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, player_width, player_height)) # Drawing player
 
-platform_X = [x*50 for x in range(10)]
-platform_y = [random.randint(10,600) for x in range(10)]
+    # for loop, drawing all the platforms, and updates there positions to the top, if they are to far down
+    for x in range(len(platform_X)):
+        pygame.draw.rect(screen, (200,200,0), (platform_X[x], platform_y[x], platform_width[x], platform_height[x])) # drawing the platform
+        platform_y[x] += platform_speed # updating the platform Y coordinat, according to the speed
 
-ready_jump = False
+        # Setting the platforms to the top, if they are under the screen
+        if(platform_y[x] > 810):
+            platform_y[x] = 0
+            platform_X[x] = random.randint(10,400)
 
-def draw(screen, x, y, player_width, player_height):
 
-    for x in range(10):
-        pygame.draw.rect(screen, (200,200,0), (platform_X[x], platform_y[x], platform_width[x], platform_height[x]))
+# checking if the player is standing on one of the platforms
+def collision(gravity, x, y, fall_speed, player_width, platform_height, platform_width, platform_X, platform_y):   
+    # 2 variables that is used
+    ready_jump = False 
+    collision_check = False
 
-    pygame.draw.rect(screen, (255, 0, 0), (x, y, player_width, player_height))
-    
+    # this for loop check all the platforms, and see if the player is on top
+    for i in range(len(platform_X)): 
+        if(y <= platform_y[i] - platform_height[i] and y >= platform_y[i] - platform_height[i] - 10 and x >= platform_X[i] - player_width and x <= platform_X[i] + platform_width[i]):
+            gravity = 0 # if the player is on top, don't let them fall down
+            collision_check = True 
 
-def collision(gravity, x, y, collision_check, fall_speed):
-    if(y < platform_y - platform_height - 7 and not y < platform_y - platform_height - 10 and x > platform_X - 15 and x < platform_X + platform_width - 5):
-        if(collision_check == False):
-            gravity = 0
-            collision_check = True
-    else:
+            # There is a little bug, for then the platforms fall down, and the player fall with
+            # half the time the player is technically not touching the platform
+            # So we just check if they are a little futher above 
+            if(y < platform_y[i]  and not y < platform_y[i] - 25): 
+                ready_jump = True
+            else:
+                ready_jump = False
+            
+    if(collision_check == False): # if the player has not touched any of the platforms, make them fall down
         if(gravity < 2):
             gravity += fall_speed
-            collision_check = False
 
-    return gravity
-      
-def jump(y):
+    return gravity, ready_jump # send the gravity and ready_jump back to the main
 
-    for x in range(10):
-        if(y < platform_y[x]  and not y < platform_y[x] - 23):
-            ready_jump = True
-        else:
-            ready_jump = False
-        return ready_jump
+# Rasmus Rosendal Nielsen
--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 import pygame
-from functions import draw,collision,jump
+from functions import * # importing the functions from the other file
 
-pygame.init()
+pygame.init() # Starting pygame
   
-screen = pygame.display.set_mode((500, 800))
+
+screen = pygame.display.set_mode((500, 800))    # Setting the window size
   
-pygame.display.set_caption("Game")
-  
-x = 200
+pygame.display.set_caption("Game") # Setting an title to the window
+
+# Player X and Y coordinates
+x = 400
 y = 100
   
+# Size of the player  
 player_width = 20
 player_height = 20
 
+platform_speed = 0.2
 
-  
 player_speed = 2
-player_jump = -3.5
-gravity = 2
-fall_speed = 0.05
+player_jump = -3.5 # jump height
+gravity = 2 # How fast the player falls
+fall_speed = 0.05 # How fast the player accelerates 
   
-collision_check = False
-ready_jump = False
+number_of_platforms = 15
+
+# Making and setting the different platforms
+platform_height = [10 for x in range(number_of_platforms)]
+platform_width = [random.randint(50,100) for x in range(number_of_platforms)]
+
+platform_X = [random.randint(10,400) for x in range(number_of_platforms)]
+platform_y = [x*50 for x in range(number_of_platforms)]
+
+score = 0
+
+# Color and font, for the score text
+font_color=(0,150,250)
+font_obj=pygame.font.SysFont("Comic Sans ms",25)
+
+ready_jump = False # Checking if the player can jump
 
 # Indicates pygame is running
 run = True
@@ -30,8 +47,10 @@ run = True
 while run:
     # creates time delay of 10ms 
     pygame.time.delay(10)
-      
-    
+
+    score += 0.1 
+    if(int(score) % 10 == 0):
+        platform_speed += 0.002
     # iterate over the list of Event objects  
     # that was returned by pygame.event.get() method.  
     for event in pygame.event.get():
@@ -54,21 +73,23 @@ while run:
          
 
     if keys[pygame.K_UP] and y>0:
-      if(ready_jump == True):
+      if(ready_jump == True): # Can the player jump
         gravity = player_jump
           
     
-    ready_jump = jump(y)
-
-    
-    screen.fill((50, 50, 50))   
+    screen.fill((50, 50, 50)) # Setting the background color
           
-    # completely fill the surface object  
-    # with black colour  
-    gravity = collision(gravity, x, y, collision_check, fall_speed)
+    # updating the gravity and ready_jump, with the collision function
+    gravity, ready_jump = collision(gravity, x, y, fall_speed, player_width, platform_height, platform_width, platform_X, platform_y)
     
-    y = y + gravity 
-    draw(screen, x, y, player_width, player_height)
+    y = y + gravity # updating the players position with gravity
+
+    # calling function to draw what should be on screen
+    draw(screen, x, y, player_width, player_height, platform_height, platform_width, platform_X, platform_y, platform_speed)
+
+    # setting the score text up
+    text_obj = font_obj.render("Score: " +str(int(score)),True,font_color)
+    screen.blit(text_obj,(50,30))
 
     # it refreshes the window
     pygame.display.update() 
@@ -76,3 +97,4 @@ while run:
 # closes the pygame window 
 pygame.quit()
 
+# Rasmus Rosendal Nielsen
